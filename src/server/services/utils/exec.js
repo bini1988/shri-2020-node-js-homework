2fe92ec10639ffae8b29f34c475cd13b87b724b9
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-03-16 14:00:44 +0300

    Coderef exec util

--- src/server/services/utils/exec.js
+++ src/server/services/utils/exec.js
@@ -1,10 +1,16 @@
 const { exec } = require('child_process');
 
-module.exports = (cmd) => {
+/**
+ * @param {string} cmd Исполняемая команда
+ * @param {string} cwd Рабочая директория
+ */
+function execute(cmd, cwd) {
   return new Promise((resolve, reject) => {
-    exec(cmd, (error, stdout) => {
+    exec(cmd, (error, stdout, stderr) => {
       if (error) { reject(error); }
-      resolve(stdout);
+      resolve(stdout || stderr);
     });
   });
-};
+}
+
+module.exports = execute;
