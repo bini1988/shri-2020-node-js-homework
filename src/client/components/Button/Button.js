dd4667312a6cdc32cbc0c444c0b6a610ff60d4dd
Nikolai <rus.nick@mail.ru>
2020-03-28 13:58:29 +0300

    Button component makrup

--- src/client/components/Button/Button.js
+++ src/client/components/Button/Button.js
@@ -1,20 +1,47 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { cn } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
+
 import './Button.scss';
 
-function Button() {
+const bn = cn('Button');
+
+function Button(props) {
+  const {
+    className, label, theme, size, view, adaptive, ...restProps
+  } = props;
+  const mods = {
+    theme, size, view, adaptive,
+  };
+
   return (
     <button
       type="button"
-      className="Button Button_theme_normal Button_size_s Button_view_tile"
+      {...restProps}
+      className={classnames(className, bn(mods))}
     >
-      <span className="Button-Wrapper">
-        <svg className="Button-Icon" width="12" height="12">
+      <span className={bn('Wrapper')}>
+        <svg className={bn('Icon')} width="12" height="12">
           <use xlinkHref="#settings" />
         </svg>
-        <span className="Button-Label">Settings</span>
+        <span className={bn('Label')}>{label}</span>
       </span>
     </button>
   );
 }
+
+Button.defaultProps = {
+  theme: 'normal',
+  size: 's',
+};
+Button.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string,
+  theme: PropTypes.string,
+  size: PropTypes.string,
+  view: PropTypes.string,
+  adaptive: PropTypes.bool,
+};
 
 export default Button;
