f525cd19effdf23925d5766642f31d5a3bcc2538
Nikolai <rus.nick@mail.ru>
2020-03-28 12:52:48 +0300

    Fix PageFooter markup

--- src/client/components/PageFooter/PageFooter.js
+++ src/client/components/PageFooter/PageFooter.js
@@ -1,28 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { cn } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
 import './PageFooter.scss';
 import Link from '../Link';
+
+const bn = cn('PageFooter');
 
 /**
  * Подвал страницы
  */
-function PageFooter() {
+function PageFooter(props) {
+  const { className } = props;
+
   return (
-    <footer className="Page-Footer PageFooter">
-      <div className="PageFooter-Container Container">
-        <ul className="PageFooter-Menu">
-          <li className="PageFooter-Item">
+    <footer className={classnames(className, bn())}>
+      <div className={classnames(bn('Container'), 'Container')}>
+        <ul className={bn('Menu')}>
+          <li className={bn('Item')}>
             <Link href="#support" label="Support" />
           </li>
-          <li className="PageFooter-Item">
+          <li className={bn('Item')}>
             <Link href="#learning" label="Learning" />
           </li>
         </ul>
-        <div className="PageFooter-Copyright">
+        <div className={bn('Copyright')}>
           &copy;2020 Your Name
         </div>
       </div>
     </footer>
   );
 }
+
+PageFooter.propTypes = {
+  className: PropTypes.string,
+};
 
 export default PageFooter;
