a5255624d044374c838f401debc349127223c816
Nikolai <rus.nick@mail.ru>
2020-03-28 13:00:35 +0300

    Fix OwnerSpan component markup

--- src/client/components/OwnerSpan/OwnerSpan.js
+++ src/client/components/OwnerSpan/OwnerSpan.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { cn } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
+
 import './OwnerSpan.scss';
 
+const bn = cn('OwnerSpan');
+
 function OwnerSpan(props) {
-  const { owner } = props;
+  const { className, owner } = props;
 
   return (
-    <span className="OwnerSpan">
-      <span className="OwnerSpan-Label">Author: </span>
-      <svg className="OwnerSpan-Icon" width="16" height="16">
+    <span className={classnames(className, bn())}>
+      <span className={bn('Label')}>Author: </span>
+      <svg className={bn('Icon')} width="16" height="16">
         <use xlinkHref="#user" />
       </svg>
-      <span className="OwnerSpan-Name">{owner}</span>
+      <span className={bn('Name')}>{owner}</span>
     </span>
   );
 }
 
 OwnerSpan.propTypes = {
+  className: PropTypes.string,
   owner: PropTypes.string.isRequired,
 };
 
