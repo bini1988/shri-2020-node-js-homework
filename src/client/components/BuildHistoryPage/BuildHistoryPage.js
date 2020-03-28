7d948c7f688fee72c207f5ae2abe3bfc7d029afa
Nikolai <rus.nick@mail.ru>
2020-03-28 15:21:09 +0300

    BuildHistoryPage markup & styles

--- src/client/components/BuildHistoryPage/BuildHistoryPage.js
+++ src/client/components/BuildHistoryPage/BuildHistoryPage.js
@@ -1,64 +1,91 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { cn } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
 
+import './BuildHistoryPage.scss';
 import PageHeader from '../PageHeader';
 import PageFooter from '../PageFooter';
+import Button from '../Button';
 import BuildCard from '../BuildCard';
+
+const bn = cn('BuildHistoryPage');
 
 /**
  * Страница 'История сборок'
  */
-function BuildHistoryPage() {
+function BuildHistoryPage({ className, history }) {
+  const handleBuild = () => history.push('/settings');
+  const handleSettings = () => history.push('/settings');
+
   return (
-    <>
-      <PageHeader view="accent">
-        <Link
-          className="Header-Button Button Button_theme_normal Button_adaptive Button_size_s"
-          to="/build-details"
+    <div className={classnames(className, bn())}>
+      <PageHeader className={bn('Header')}>
+        <PageHeader.Title accent>
+          School CI server
+        </PageHeader.Title>
+        <PageHeader.Aside
+          className={bn('Aside')}
         >
-          <span className="Button-Wrapper">
-            <svg className="Button-Icon" width="12" height="12">
-              <use xlinkHref="#play" />
-            </svg>
-            <span className="Button-Label">{'Run build'}</span>
-          </span>
-        </Link>
-        <Link
-          className="Header-Button Button Button_theme_normal Button_size_s Button_view_tile"
-          to="/settings"
-        >
-          <span className="Button-Wrapper">
-            <svg className="Button-Icon" width="12" height="12">
-              <use xlinkHref="#settings" />
-            </svg>
-            <span className="Button-Label">{'Settings'}</span>
-          </span>
-        </Link>
+          <Button
+            adaptive
+            label="Run build"
+            iconName="play"
+            size="s"
+            onClick={handleBuild}
+          />
+          <Button
+            label="Settings"
+            iconName="settings"
+            size="s"
+            view="tile"
+            onClick={handleSettings}
+          />
+        </PageHeader.Aside>
       </PageHeader>
-      <main className="Page-Main Main">
-        <div className="Main-Container Container">
-          <section className="Main-BuildHistoryPage BuildHistoryPage">
-            <h3 className="BuildHistoryPage-Title BuildHistoryPage-Title_hidden">{'BuildHistoryPage'}</h3>
-            <ul className="BuildHistoryPage-Items">
+      <main className={bn('Main')}>
+        <div className={classnames(bn('Container'), 'Container')}>
+          <section className={bn('History')}>
+            <h3 className={bn('Title')}>
+              BuildHistoryPage
+            </h3>
+            <ul className={bn('Items')}>
               {Array.from({ length: 9 }).map((id) => (
-                <li className="BuildHistoryPage-Item" key={id}>
-                  <BuildCard />
+                <li className={bn('Item')} key={id}>
+                  <BuildCard
+                    interactive
+                    card={{
+                      status: 'Waiting',
+                      buildNumber: 1368,
+                      commitMessage: 'add documentation for postgres scaler',
+                      branchName: 'master',
+                      commitHash: '9c9f0b9',
+                      authorName: 'Philip Kirkorov',
+                      start: '2020-01-21T03:06:00.000',
+                    }}
+                  />
                 </li>
               ))}
             </ul>
-            <button className="BuildHistoryPage-More Button Button_theme_normal Button_size_s" type="button">
-              <span className="Button-Wrapper">
-                <span className="Button-Label">
-                  {'Show more'}
-                </span>
-              </span>
-            </button>
+            <Button
+              className={bn('More')}
+              label="Show more"
+              size="s"
+              onClick={handleSettings}
+            />
           </section>
         </div>
       </main>
       <PageFooter />
-    </>
+    </div>
   );
 }
+
+BuildHistoryPage.propTypes = {
+  className: PropTypes.string,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default BuildHistoryPage;
