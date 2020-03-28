c54c5a4d03633d8d4fd50bdb0d5ab182068374b1
Nikolai <rus.nick@mail.ru>
2020-03-28 12:41:36 +0300

    BuildCard component markup & styles

--- src/client/components/BuildCard/BuildCard.js
+++ src/client/components/BuildCard/BuildCard.js
@@ -1,58 +1,101 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { cn } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
+
 import './BuildCard.scss';
 import CommitSpan from '../CommitSpan';
 import OwnerSpan from '../OwnerSpan';
 import DateTime from '../DateTime';
 
+const bn = cn('BuildCard');
+
 /**
  * Карточка сборки
  */
-function BuildCard() {
+function BuildCard(props) {
+  const { className, interactive, card = {} } = props;
+  const {
+    status,
+    buildNumber = '--',
+    commitMessage,
+    branchName,
+    commitHash,
+    authorName,
+    start,
+    duration,
+  } = card;
+
   return (
-    <article className="BuildCard">
-      <div className="BuildCard-Wrapper">
-        <div className="BuildCard-Status Status Status_type_success">
-          <svg className="BuildCard-Icon" width="24" height="24">
+    <article className={classnames(className, bn({ status, interactive }))}>
+      <div className={bn('Wrapper')}>
+        <div className={bn('Status')}>
+          <svg className={bn('Icon')} width="24" height="24">
             <use xlinkHref="#done" />
           </svg>
-          Success
+          {status}
         </div>
-        <div className="BuildCard-Content">
-          <h3 className="BuildCard-Title">
-            <span className="BuildCard-No Status Status_type_success">
-              #1368
+        <div className={bn('Content')}>
+          <h3 className={bn('Title')}>
+            <span className={bn('No')}>
+              {`#${buildNumber}`}
             </span>
-            <span className="BuildCard-Text">
-              add documentation for postgres scaler
+            {' '}
+            <span className={bn('Text')}>
+              {commitMessage}
             </span>
           </h3>
-          <div className="BuildCard-Meta">
+          <div className={bn('Meta')}>
             <CommitSpan
-              className="BuildCard-Commit"
-              branch="master"
-              hash="9c9f0b9"
+              className={bn('Commit')}
+              branch={branchName}
+              hash={commitHash}
             />
             <OwnerSpan
-              className="BuildCard-Owner"
-              owner="Philip Kirkorov"
+              className={bn('Owner')}
+              owner={authorName}
             />
           </div>
         </div>
-        <div className="BuildCard-Aside">
+        <div className={bn('Aside')}>
           <DateTime
-            className="BuildCard-Time"
+            className={bn('Time')}
             view="date"
-            dateTime="2020-01-21T03:06:00.000"
+            dateTime={start}
           />
           <DateTime
-            className="BuildCard-Time"
+            className={bn('Time')}
             view="time"
-            dateTime="01:20"
+            dateTime={duration}
           />
         </div>
       </div>
     </article>
   );
 }
+
+BuildCard.propTypes = {
+  className: PropTypes.string,
+  /** Карточка интерактивна */
+  interactive: PropTypes.bool,
+  /** Параметры карточки сборки */
+  card: PropTypes.shape({
+    id: PropTypes.string,
+    status: PropTypes.oneOf([
+      'Waiting',
+      'InProgress',
+      'Success',
+      'Fail',
+      'Canceled',
+    ]),
+    buildNumber: PropTypes.number,
+    commitMessage: PropTypes.string,
+    branchName: PropTypes.string,
+    commitHash: PropTypes.string,
+    authorName: PropTypes.string,
+    start: PropTypes.string,
+    duration: PropTypes.number,
+  }).isRequired,
+};
 
 export default BuildCard;
