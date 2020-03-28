21bededb12958c35df296bc89a3cb58005ba49a2
Nikolai <rus.nick@mail.ru>
2020-03-28 16:38:48 +0300

    NumericInput markup & styles

--- src/client/components/NumericInput/NumericInput.js
+++ src/client/components/NumericInput/NumericInput.js
@@ -1,39 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { cn } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
+
 import './NumericInput.scss';
 import Input from '../Input';
 
+const bn = cn('NumericInput');
+
 function NumericInput(props) {
   const {
-    id, name, label, units,
+    className, id, name, label, palceholder, units,
   } = props;
 
   return (
-    <div className="NumericInput">
-      <div className="NumericInput-Wrapper">
-        <label className="NumericInput-Label" htmFor={id}>
-          {label}
-        </label>
+    <div className={classnames(className, bn())}>
+      <div className={bn('Wrapper')}>
+        {!!label && (
+          <label className={bn('Label')} htmlFor={id}>
+            {label}
+          </label>
+        )}
         <Input
-          className="NumericInput-Control"
+          className={bn('Control')}
           id={id}
           name={name}
+          palceholder={palceholder}
           type="number"
           min="0"
           max="1000"
           textAlign="right"
-          cleanable
         />
-        <span className="NumericInput-Units">{units}</span>
+        <span className={bn('Units')}>{units}</span>
       </div>
     </div>
   );
 }
 
 NumericInput.propTypes = {
+  className: PropTypes.string,
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  palceholder: PropTypes.string,
   units: PropTypes.string.isRequired,
 };
 
