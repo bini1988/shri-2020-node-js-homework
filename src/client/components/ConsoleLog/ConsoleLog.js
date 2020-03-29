83c743be58b17b97a33bd55abe7265e92802a81d
Nikolai <rus.nick@mail.ru>
2020-03-29 12:14:21 +0300

    Add ConsoleLog component

---
+++ src/client/components/ConsoleLog/ConsoleLog.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-danger */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { cn } from '@bem-react/classname';
+import { classnames } from '@bem-react/classnames';
+import Convert from 'ansi-to-html';
+import './ConsoleLog.scss';
+
+const bn = cn('ConsoleLog');
+const convert = new Convert({ fg: '#000', bg: '#000' });
+
+function ConsoleLog(props) {
+  const { className, children } = props;
+  const html = children ? convert.toHtml(children) : null;
+
+  return (
+    <div className={classnames(className, bn())}>
+      <pre className={bn('Wrappper')} dangerouslySetInnerHTML={{ __html: html }} />
+    </div>
+  );
+}
+
+ConsoleLog.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.string,
+};
+
+export default ConsoleLog;
