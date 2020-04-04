9f508a9b96502e2aead8868f76b494a0a56a0f7c
Nikolai <rus.nick@mail.ru>
2020-04-05 01:28:23 +0300

    Add server mocks for build log

---
+++ src/server/services/mocks/mock-build-log.js
@@ -0,0 +1,88 @@
+/* eslint-disable no-useless-escape */
+module.exports = `
+[1mWebpack Bundle Analyzer[22m saved report to [1mD:\Projects\web\shri-2020-node-js-homework\static\bundle.report.html[22m
+  Hash: [1m737299fb79aff0eb0673[39m[22m
+  Version: webpack [1m4.42.1[39m[22m
+  Time: [1m4498[39m[22mms
+  Built at: 2020-03-29 [1m11:23:00[39m[22m
+                   [1mAsset[39m[22m      [1mSize[39m[22m  [1mChunks[39m[22m  [1m[39m[22m           [1m[39m[22m[1mChunk Names[39m[22m
+       [1m[32mmain.chunk.min.js[39m[22m  40.1 KiB       [1m0[39m[22m  [1m[32m[emitted][39m[22m  main
+      [1m[32mmain.style.min.css[39m[22m  14.9 KiB       [1m0[39m[22m  [1m[32m[emitted][39m[22m  main
+  [1m[32mmanifest.bundle.min.js[39m[22m  1.46 KiB       [1m1[39m[22m  [1m[32m[emitted][39m[22m  manifest
+    [1m[32mvendors.chunk.min.js[39m[22m   214 KiB       [1m2[39m[22m  [1m[32m[emitted][39m[22m  vendors
+  Entrypoint [1mmain[39m[22m [1m[33m[big][39m[22m = [1m[32mmanifest.bundle.min.js[39m[22m [1m[32mvendors.chunk.min.js[39m[22m [1m[32mmain.style.min.css[39m[22m [1m[32mmain.chunk.min.js[39m[22m
+  [LhCv] [1m./node_modules/history/esm/history.js + 2 modules[39m[22m 30.6 KiB {[1m[33m2[39m[22m}[1m[32m [built][39m[22m
+      |    3 modules
+  [O14P] [1m./src/client/index.js + 44 modules[39m[22m 57.8 KiB {[1m[33m0[39m[22m}[1m[32m [built][39m[22m
+      | [1m./src/client/index.js[39m[22m 191 bytes[1m[32m [built][39m[22m
+      |     + 44 hidden modules
+  [Ty5D] [1m./node_modules/react-router/esm/react-router.js + 1 modules[39m[22m 29.2 KiB {[1m[33m2[39m[22m}[1m[32m [built][39m[22m
+      |    2 modules
+  [qVcC] [1m./src/client/common/sass/blocks.scss[39m[22m 39 bytes {[1m[33m0[39m[22m}[1m[32m [built][39m[22m
+  [yLpj] [1m(webpack)/buildin/global.js[39m[22m 472 bytes {[1m[33m2[39m[22m}[1m[32m [built][39m[22m
+      + 94 hidden modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/common/sass/blocks.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+      [I9FZ] [1m./node_modules/css-loader/dist/cjs.js??ref--5-1!./node_modules/postcss-loader/src??ref--5-2!./node_modules/resolve-url-loader??ref--5-3!./node_modules/sass-loader/dist/cjs.js??ref--5-4!./src/client/common/sass/blocks.scss[39m[22m 7.71 KiB {[1m[33m0[39m[22m}[1m[32m [built][39m[22m
+          + 1 hidden module
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/BuildCard/BuildCard.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/BuildDetailsPage/BuildDetailsPage.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/BuildHistoryPage/BuildHistoryPage.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/Button/Button.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/CommitSpan/CommitSpan.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/ConsoleLog/ConsoleLog.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/DateTime/DateTime.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/Input/Input.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/Link/Link.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/MainPage/MainPage.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/NewBuildDialog/NewBuildDialog.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/NewBuildForm/NewBuildForm.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/NumericInput/NumericInput.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/OwnerSpan/OwnerSpan.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/PageFooter/PageFooter.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/PageHeader/PageHeader.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/SettingsForm/SettingsForm.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/SettingsPage/SettingsPage.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/TextInput/TextInput.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+  Child [1mmini-css-extract-plugin node_modules/css-loader/dist/cjs.js??ref--5-1!node_modules/postcss-loader/src/index.js??ref--5-2!node_modules/resolve-url-loader/index.js??ref--5-3!node_modules/sass-loader/dist/cjs.js??ref--5-4!src/client/components/Title/Title.scss[39m[22m:
+      Entrypoint [1mmini-css-extract-plugin[39m[22m = [1m[32m*[39m[22m
+         2 modules
+`;
