5c02d2d64eabb6b02e6c17b6080dbeb028130ae2
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-04-12 17:39:12 +0300

    Coderef IndexPage component connect

--- src/client/components/IndexPage/index.js
+++ src/client/components/IndexPage/index.js
@@ -1,10 +1,1 @@
-import { connect } from 'react-redux';
-import IndexPageView from './IndexPage';
-import { getSettingsValues, fetchSettings } from '../../services/redux/reducer/settings';
-
-function mapStateToProps(state) {
-  const settings = getSettingsValues(state);
-  return { settings };
-}
-
-export default connect(mapStateToProps, { fetchSettings })(IndexPageView);
+export { default } from './IndexPage';
