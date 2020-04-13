468c969399bcfd220ef0c5b779a6c1fa109ee320
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-04-13 23:01:09 +0300

    Add settings form submit e2e test

--- tests/e2e/settings-page.js
+++ tests/e2e/settings-page.js
@@ -1,4 +1,5 @@
 const assert = require('chai').assert;
+const mockSettings =require('../../hermione/mocks/settings');
 
 describe('Страница настроек', function() {
   beforeEach(async function () {
@@ -16,6 +17,48 @@ describe('Страница настроек', function() {
       .url(this.url)
       .waitForVisible('[data-test="settings-page"]')
       .isExisting('[data-test="settings-form"]')
+      .then(assert.isTrue);
+  });
+  it('Форму нельзя отправить если не заполнены обязательные поля', function() {
+
+    return this.browser
+      .url(this.url)
+      .waitForVisible('[data-test="settings-page"]')
+      .$('[data-test="settings-form"] [data-test="submit"]')
+      .getAttribute('disabled')
+      .then(value => assert.equal(value, 'true'));
+  });
+  it.only('Отправка формы после заполнения полей', async function() {
+    await this.browser
+      .url(this.url)
+      .waitForVisible('[data-test="settings-page"]');
+
+    await this.browser
+      .$('[data-test="repoName"] [data-test="input"]')
+      .setValue(mockSettings.repoName);
+
+    await this.browser
+      .$('[data-test="buildCommand"] [data-test="input"]')
+      .setValue(mockSettings.buildCommand);
+
+    await this.browser
+      .$('[data-test="mainBranch"] [data-test="input"]')
+      .setValue(mockSettings.mainBranch);
+
+    await this.browser
+      .$('[data-test="period"] [data-test="input"]')
+      .setValue(mockSettings.period);
+
+    await this.browser
+      .$('[data-test="settings-form"] [data-test="submit"]')
+      .getAttribute('disabled')
+      .then(value => assert.equal(value, null));
+
+    await this.browser
+      .$('[data-test="settings-form"] [data-test="submit"]')
+      .click()
+      .waitForVisible('[data-test="build-history-page"]', 5000)
+      .isExisting('[data-test="build-history-page"]')
       .then(assert.isTrue);
   });
 });
