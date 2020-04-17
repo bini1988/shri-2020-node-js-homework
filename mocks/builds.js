9e8e829377129adf7b064349f33868829c14e322
Nikolai Karastelev <n.karastelev@credinform.ru>
2020-04-17 14:10:21 +0300

    Add builds mock & api/builds method test

---
+++ mocks/builds.js
@@ -0,0 +1,60 @@
+/**
+ * Mock объект массива сборок
+ */
+module.exports = [
+  {
+      id: "0d844533-bf03-45c1-b161-cb9be18df521",
+      configurationId: "1d73c0ea-29ad-4067-8dfd-ed6b172d4112",
+      buildNumber: 5,
+      commitMessage: "Fix solution test",
+      commitHash: "96233c2",
+      branchName: "master",
+      authorName: "Nikolai",
+      status: "InProgress",
+      start: "2020-04-17T10:19:53.317"
+  }, {
+      id: "a8711d33-cc4c-4701-9162-26cf4c724343",
+      configurationId: "1d73c0ea-29ad-4067-8dfd-ed6b172d4112",
+      buildNumber: 4,
+      commitMessage: "Fix solution test",
+      commitHash: "96233c2",
+      branchName: "master",
+      authorName: "Nikolai",
+      status: "Success",
+      start: "2020-04-17T10:19:46.183",
+      "duration": 5996908
+  }, {
+      id: "5b4f330c-b00c-46d8-80c5-e0859d38e911",
+      configurationId: "1d73c0ea-29ad-4067-8dfd-ed6b172d4112",
+      buildNumber: 3,
+      commitMessage: "Fix solution test",
+      commitHash: "96233c2",
+      branchName: "master",
+      authorName: "Nikolai",
+      status: "Success",
+      start: "2020-04-17T10:08:35.75",
+      "duration": 1287378
+  }, {
+      id: "2f46bd06-5de3-42ad-8827-03e1d626a9a8",
+      configurationId: "1d73c0ea-29ad-4067-8dfd-ed6b172d4112",
+      buildNumber: 2,
+      commitMessage: "Fix solution test",
+      commitHash: "96233c2",
+      branchName: "master",
+      authorName: "Nikolai",
+      status: "Fail",
+      start: "2020-04-17T09:56:15.368",
+      "duration": 5796364
+  }, {
+      id: "0610ff77-5215-45e0-b3d9-4fc10987d6dc",
+      configurationId: "1d73c0ea-29ad-4067-8dfd-ed6b172d4112",
+      buildNumber: 1,
+      commitMessage: "Fix solution test",
+      commitHash: "96233c2",
+      branchName: "master",
+      authorName: "Nikolai",
+      status: "Success",
+      start: "2020-04-17T09:55:29.119",
+      "duration": 5013935
+  }
+];
